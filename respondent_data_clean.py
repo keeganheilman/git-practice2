6ddda052b6dc8f9c6964684f8064a68dc861108a
Keegan Heilman <hkeegan@umich.edu>
2021-03-12 00:11:44 +0000

    completed all function operations

--- respondent_data_clean.py
+++ respondent_data_clean.py
@@ -4,27 +4,26 @@ import pandas as pd
 
 
 def get_data(input_file):
-    print('Importing Data from %s...' % (input_file))
+    print('Importing data from %s...' % (input_file))
     df = pd.read_csv(input_file)
-    df.name = str(input_file)
     return df
 
 
 def join_data(df_1, df_2):
-    #print('Joining Data from %s and %s' % (df_1.name, df_2.name))
+    print('Joining data...')
     return df_1.join(df_2.set_index('respondent_id'), on ='respondent_id')
 
 
 def clean_data(df):
-    # print('Cleaning Data of %s...' % (df.name))
+    print('Cleaning data...')
     df['birthdate'] = pd.to_datetime(df['birthdate'], format='%Y-%m-%d')
-    df[['respondent_id', 'name', 'address', 'phone', 'job', 'company', 'birthdate']]
+    assert all(df.columns == ['respondent_id','name', 'address', 'phone', 'job', 'company', 'birthdate']), "Incorrect column names in the Pandas DataFrame."
     return df
 
 
 def export_data(df, output_file):
-    #print('Exporting Data to %s...' % (output_file))
-    df.to_csv(output_file)
+    print('Exporting data to %s...' % (output_file))
+    df.to_csv(output_file, date_format = '%Y-%m-%d')
     return 0
 
 
@@ -40,14 +39,12 @@ if __name__ == '__main__':
     df_1 = get_data(args.contact_info_file)
     df_2 = get_data(args.other_info_file)
 
-    df = join_data(df_1, df_2)
-    df.name = args.output_file
-    
+    df = join_data(df_1, df_2)    
     df_clean = clean_data(df)
 
-    export_data(df_clean, args.output_file)
+    if (export_data(df_clean, args.output_file) == 0):
+        print('Completed successfully.')
 
-    print('Completed.')
 
 
 
