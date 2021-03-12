cf0ccf6cd26975e6b251615ae976abf59ec95201
Keegan Heilman <hkeegan@umich.edu>
2021-03-12 01:54:44 +0000

    zfill for accurate birthdate parsing

--- respondent_data_clean.py
+++ respondent_data_clean.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
 
-
-
 def get_data(input_file):
     print('Importing data from %s...' % (input_file))
     df = pd.read_csv(input_file)
@@ -11,11 +9,15 @@ def get_data(input_file):
 
 def join_data(df_1, df_2):
     print('Joining data...')
-    return df_1.join(df_2.set_index('respondent_id'), on ='respondent_id')
+    assert len(df_1) == len(df_2), "DataFrames do not contain the same number of individuals"
+    df_joined = df_1.join(df_2.set_index('respondent_id'), on ='respondent_id')
+    assert len(df_joined) == len(df_1), "Joined DataFrames do not match along all respondent_ids"
+    return df_joined
 
 
 def clean_data(df):
     print('Cleaning data...')
+    df['birthdate'] = df['birthdate'].apply(lambda bdate: str(bdate).zfill(8))
     df['birthdate'] = pd.to_datetime(df['birthdate'], format='%m%d%Y')
     assert all(df.columns == ['respondent_id','name', 'address', 'phone', 'job', 'company', 'birthdate']), "Incorrect column names in the Pandas DataFrame."
     return df
@@ -23,7 +25,7 @@ def clean_data(df):
 
 def export_data(df, output_file):
     print('Exporting data to %s...' % (output_file))
-    df.to_csv(output_file, index=False)
+    df.to_csv(output_file,date_format='%Y-%m-%d', index=False)
     return 0
 
 
